79ba2ebd8f507bc821d80ebfc79d1a479d1ebb29
hoshikon <hoshikong@gmail.com>
2019-05-23 00:49:11 +0100

    Complete assignment - Binary Trees

--- actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -47,6 +47,7 @@ object BinaryTreeSet {
   /** Message to signal successful completion of an insert or remove operation. */
   case class OperationFinished(id: Int) extends OperationReply
 
+  case object DigestQueue
 }
 
 
@@ -66,14 +67,52 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case op: Operation => root ! op
+    case GC => doGC()
+  }
+
+  val digesting: Receive = {
+    case op: Operation =>
+      pendingQueue = pendingQueue.enqueue(op)
+    case DigestQueue =>
+      if (pendingQueue.isEmpty) {
+        context.become(normal)
+      } else {
+        val (op, rest) = pendingQueue.dequeue
+        root ! op
+        pendingQueue = rest
+        self ! DigestQueue
+      }
+    case GC => doGC()
+
+  }
 
   // optional
   /** Handles messages while garbage collection is performed.
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case op: Operation =>
+      pendingQueue = pendingQueue.enqueue(op)
+    case CopyFinished =>
+      root ! PoisonPill
+      root = newRoot
+      if (pendingQueue.isEmpty) {
+        context.become(normal)
+      } else {
+        context.become(digesting)
+        self ! DigestQueue
+      }
+    case GC => ()
+  }
+
+  private def doGC() = {
+    val newRoot = createRoot
+    context.become(garbageCollecting(newRoot))
+    root ! CopyTo(newRoot)
+  }
 
 }
 
@@ -101,13 +140,72 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case Insert(req, id, e) =>
+      if (e == elem) {
+        removed = false
+        req ! OperationFinished(id)
+      } else {
+        getSubtree(e) match {
+          case Some(st) =>
+            st ! Insert(req, id, e)
+          case None =>
+            subtrees += (lOrR(e) -> context.actorOf(BinaryTreeNode.props(e, false)))
+            req ! OperationFinished(id)
+        }
+      }
+
+    case Contains(req, id, e) =>
+      if (e == elem) {
+        req ! ContainsResult(id, !removed)
+      } else {
+        getSubtree(e) match {
+          case Some(st) => st ! Contains(req, id, e)
+          case None => req ! ContainsResult(id, false)
+        }
+      }
+
+    case Remove(req, id, e) =>
+      if (e == elem) {
+        removed = true
+        req ! OperationFinished(id)
+      } else {
+        getSubtree(e) match {
+          case Some(st) => st ! Remove(req, id, e)
+          case None => req ! OperationFinished(id)
+        }
+      }
+
+    case CopyTo(tree) =>
+      val expected = subtrees.values.toSet + self
+      context.become(copying(expected))
+
+      if (!removed) {
+        tree ! Insert(self, 0, elem)
+      } else {
+        self ! OperationFinished(0)
+      }
+      subtrees.values.foreach(_ ! CopyTo(tree))
+  }
+
+  private def lOrR(e: Int): Position = if (e < elem) Left else Right
+  private def getSubtree(e: Int): Option[ActorRef] = subtrees.get(lOrR(e))
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  def copying(expected: Set[ActorRef]): Receive = {
+    case OperationFinished(_) => finishOrBecome(expected - self)
+    case CopyFinished => finishOrBecome(expected - sender())
+  }
 
+  private def finishOrBecome(expected: Set[ActorRef]): Unit = {
+    if (expected.isEmpty) {
+      context.parent ! CopyFinished
+    } else {
+      context.become(copying(expected))
+    }
+  }
 
 }
